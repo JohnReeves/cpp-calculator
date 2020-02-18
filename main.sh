3df59505b6af89353b686872e6bc8bd43e9b7c71
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2020-02-18 20:09:33 +0000

    powers working

--- main.sh
+++ main.sh
@@ -20,7 +20,10 @@ echo '+------------------+'
 echo ' '
 
 # g++ -o rpn-calc-regex2 rpn-calc-regex1.cpp
-clang++ -std=c++11 -o rpn-calc-regex1 rpn-calc-regex1.cpp
+clang++ -std=c++11 -o rpn-calc-regex2 rpn-calc-regex2.cpp
 ./rpn-calc-regex1 '15 7 3 1 + − ÷ 3 × 3 4 1 + + -' # 20
 
+./rpn-calc-regex2 '10 0 ^' # 1
+./rpn-calc-regex2 '10 1 ^' # 10
+./rpn-calc-regex2 '10 3 ^' # 1000
 
--- rpn-calc-regex2.cpp
+++ rpn-calc-regex2.cpp
@@ -23,7 +23,7 @@ using namespace std;
 stack<double> st;
 string instr;
 string num_pattern("(\\+|-)?[0-9]+(\\.[0-9]*)?");
-string op_pattern("[+*/-]");
+string op_pattern("(\\*|\\/|\\+|\\-|\\^)");
 regex re(num_pattern + "|" + op_pattern);
 
 int main(int argc, char **argv) {
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
     for (;it != it_end; ++it) {
       // cout << it->str().c_str() << "   ";
 
-      if (it->str().find_first_not_of("+*/-") != it->str().npos) {
+      if (it->str().find_first_not_of("+*/-^") != it->str().npos) {
         st.push(atof(it->str().c_str()));
       } else {
         double op2 = st.top(); st.pop();
@@ -48,6 +48,7 @@ int main(int argc, char **argv) {
           case '-': st.push( op1 - op2 ); break;
           case '*': st.push( op1 * op2 ); break;
           case '/': st.push( op1 / op2 ); break;
+          case '^': st.push( pow(op1, op2) ); break;
         }
       }
     }
